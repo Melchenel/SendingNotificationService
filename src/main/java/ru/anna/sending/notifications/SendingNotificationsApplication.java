0a6d372a517c8dde6323931c8547a6a9bc335959
Anna <anna.ryaskina@masterdata.ru>
2022-09-14 15:40:45 +0300

    first commit

---
+++ src/main/java/ru/anna/sending/notifications/SendingNotificationsApplication.java
@@ -0,0 +1,13 @@
+package ru.anna.sending.notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendingNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendingNotificationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/anna/sending/notifications/SendingNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.anna.sending.notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendingNotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
